3b100e1537fa830b42daaa0c7e90a909c50d207b
emileyski <96009930+emileyski@users.noreply.github.com>
2024-01-14 16:26:09 +0200

    refactor: brought skills and education to the array

--- src/Pages/EducationPage.jsx
+++ src/Pages/EducationPage.jsx
@@ -5,6 +5,27 @@ import WorkEducContent from "../Components/WorkEducContent/WorkEducContent";
 import { useNotebook } from "../Contexts/NotebookContext";
 import PageNumber from "../Components/PageNumber";
 
+const educationInfo = [
+  {
+    year: "2020 - 2021",
+    title: "Web Developer - Company",
+    content:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat odio eos assumenda consectetur, numquam officiis dicta fuga consequuntur possimus harum fugiat vel nemo? Ab, hic.",
+  },
+  {
+    year: "2020 - 2021",
+    title: "Web Developer - Company",
+    content:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat odio eos assumenda consectetur, numquam officiis dicta fuga consequuntur possimus harum fugiat vel nemo? Ab, hic.",
+  },
+  {
+    year: "2020 - 2021",
+    title: "Web Developer - Company",
+    content:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat odio eos assumenda consectetur, numquam officiis dicta fuga consequuntur possimus harum fugiat vel nemo? Ab, hic.",
+  },
+];
+
 function EducationPage() {
   const { isMobile } = useNotebook();
 
@@ -14,31 +35,16 @@ function EducationPage() {
         Education <i className="bx bxs-book"></i>
       </Title>
 
-      <WorkEducBox>
-        <WorkEducContent
-          year={"2020 - 2021"}
-          title={"Web Developer - Company"}
-          content={
-            "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat odio eos assumenda consectetur, numquam officiis dicta fuga consequuntur possimus harum fugiat vel nemo? Ab, hic."
-          }
-        />
-
-        <WorkEducContent
-          year={"2020 - 2021"}
-          title={"Web Developer - Company"}
-          content={
-            "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat odio eos assumenda consectetur, numquam officiis dicta fuga consequuntur possimus harum fugiat vel nemo? Ab, hic."
-          }
-        />
-
-        <WorkEducContent
-          year={"2020 - 2021"}
-          title={"Web Developer - Company"}
-          content={
-            "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quaerat odio eos assumenda consectetur, numquam officiis dicta fuga consequuntur possimus harum fugiat vel nemo? Ab, hic."
-          }
-        />
-      </WorkEducBox>
+      {educationInfo.map((info, index) => (
+        <WorkEducBox key={index}>
+          <WorkEducContent
+            key={index}
+            year={info.year}
+            title={info.title}
+            content={info.content}
+          />
+        </WorkEducBox>
+      ))}
       {isMobile && <NextPrevButton />}
       <NextPrevButton isNext={false} />
       <PageNumber number={2} />
--- src/Pages/MySkillsPage.jsx
+++ src/Pages/MySkillsPage.jsx
@@ -4,6 +4,82 @@ import Title from "../Components/Title";
 import { useNotebook } from "../Contexts/NotebookContext";
 import PageNumber from "../Components/PageNumber";
 
+const backEndSkills = [
+  {
+    name: "Node",
+    icon: "bx bxl-nodejs",
+    img: null,
+  },
+  {
+    name: "Express",
+    icon: "bx bx-coffee",
+    img: null,
+  },
+  {
+    name: "Nest",
+    icon: null,
+    img: "nest-js-icon.svg",
+  },
+  {
+    name: "TypeScript",
+    icon: "bx bxl-typescript",
+    img: null,
+  },
+  {
+    name: "AWS",
+    icon: "bx bxl-aws",
+    img: null,
+  },
+  {
+    name: "PostgreSQL",
+    icon: "bx bxl-postgresql",
+    img: null,
+  },
+  {
+    name: "MongoDB",
+    icon: "bx bxl-mongodb",
+    img: null,
+  },
+  {
+    name: "GraphQL",
+    icon: "bx bxl-graphql",
+    img: null,
+  },
+];
+
+const deploymentSkills = [
+  {
+    name: "Docker",
+    icon: "bx bxl-docker",
+    img: null,
+  },
+  {
+    name: "Kubernetes",
+    icon: "bx bxl-kubernetes",
+    img: null,
+  },
+  {
+    name: "CI/CD",
+    icon: "bx bx-package",
+    img: null,
+  },
+  {
+    name: "Git",
+    icon: "bx bxl-git",
+    img: null,
+  },
+  {
+    name: "Linux",
+    icon: null,
+    img: "linux-svgrepo-com.svg",
+  },
+  {
+    name: "NGINX",
+    icon: null,
+    img: "nginx-svgrepo-com.svg",
+  },
+];
+
 function MySkillsPage() {
   const { isMobile } = useNotebook();
 
@@ -16,57 +92,26 @@ function MySkillsPage() {
         <div className={styles["skills-content"]}>
           <h3>Back-end</h3>
           <div className={styles["content"]}>
-            <span>
-              <i className="bx bxl-nodejs"></i>Node
-            </span>
-            <span>
-              <i className="bx bx-coffee"></i>Express
-            </span>
-            <span>
-              <img src="nest-js-icon.svg" alt="" />
-              Nest
-            </span>
-            <span>
-              <i className="bx bxl-typescript"></i>TypeScript
-            </span>
-            <span>
-              <i className="bx bxl-aws"></i>AWS
-            </span>
-            <span>
-              <i className="bx bxl-postgresql"></i>PostgreSQL
-            </span>
-            <span>
-              <i className="bx bxl-mongodb"></i>MongoDB
-            </span>
-            <span>
-              <i className="bx bxl-graphql"></i>GraphQL
-            </span>
+            {backEndSkills.map((skill, index) => (
+              <span key={index}>
+                {skill.icon && <i className={skill.icon}></i>}
+                {skill.img && <img src={skill.img} alt="" />}
+                {skill.name}
+              </span>
+            ))}
           </div>
         </div>
 
         <div className={styles["skills-content"]}>
           <h3>Deployment</h3>
           <div className={styles["content"]}>
-            <span>
-              <i className="bx bxl-docker"></i>Docker
-            </span>
-            <span>
-              <i className="bx bxl-kubernetes"></i>Kubernetes
-            </span>
-            <span>
-              <i className="bx bx-package"></i>CI/CD
-            </span>
-            <span>
-              <i className="bx bxl-git"></i>Git
-            </span>
-            <span>
-              <img src="linux-svgrepo-com.svg" alt="" />
-              Linux
-            </span>
-            <span>
-              <img src="nginx-svgrepo-com.svg" alt="" />
-              NGINX
-            </span>
+            {deploymentSkills.map((skill, index) => (
+              <span key={index}>
+                {skill.icon && <i className={skill.icon}></i>}
+                {skill.img && <img src={skill.img} alt="" />}
+                {skill.name}
+              </span>
+            ))}
           </div>
         </div>
       </div>
